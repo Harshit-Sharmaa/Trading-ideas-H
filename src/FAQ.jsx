6623197b5cb77b256fb96f07138d9946263930c9
Harshit Sharma <sharmaharshit552@gmail.com>
2022-01-29 19:41:19 +0530

    saved

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -4,13 +4,13 @@ function FAQ({ question }) {
   function changeView() {
     console.log("change view function have been clicked");
     //setstate(prev=> prev= !prev);
-    setstate(!state) 
+    setstate(!state)
   }
   console.count(question)
   return (
     <div className="faq-question">
       <div className="question-body">
-        <h3> <img src="./photos/bluebox.png" className='bluebox' />{question.question}</h3>
+        <h3 id={question.class}> <img src="./photos/bluebox.png" className='bluebox' />{question.question}</h3>
         <p onClick={changeView}>{state ? <i class={"fas fa-chevron-right"}></i> : <i class="fas fa-chevron-up" style={{ color: "#0189DF" }}></i>}</p>
       </div>
       <p className={state ? "hidePtag" : "showPtag"}>{question.answer}</p>
--- src/QuestionAndAnswer.jsx
+++ src/QuestionAndAnswer.jsx
@@ -5,18 +5,22 @@ function QuestionAndAnswer() {
 
   let faQArrays = [
     {
+      class:"one",
       question: "Who can take this course?",
       answer: "This course is designed for stock market newbies, beginners and experts alike. We will cover all the concepts, from basics to advanced, required for successfully implementing the winning framework."
     },
     {
+      class:"two",
       question: "Do I have to attend all of the live sessions in real-time?",
       answer: "It's okay if you have to miss a workshop or two. We record every live workshop and make it available for you to replay, at your convenience. With that said, we strongly recommend you make time for them so you can ask questions directly to Jaspreet and join productive breakout discussions. "
     },
     {
+      iclass:"three",
       question: "What is the bootcamp fee?",
       answer: "The fee of bootcamp is Rs. 10000 inclusive of taxes. Once you enrol in this course, you get unlimited access to all the class recordings of the bootcamp. "
     },
     {
+      iclass:"four",
       question: "Will I be given buy and sell recommendations?",
       answer: "No. We are not a SEBI registered equity advisor and hence we will only be teaching you how to do profitable swing trading."
     },
